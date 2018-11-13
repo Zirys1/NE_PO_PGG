8c1df31d543d422527821ec85f17140b579e5d22
Bruns <BAR3357@uni-hamburg.de>
2018-11-13 12:38:35 +0100

    different identification of NE and publish to shinyapps.io

--- ne_po_pgg/app.R
+++ ne_po_pgg/app.R
@@ -9,7 +9,6 @@
 
 library(shiny)
 library(ggplot2)
-library(plotly)
 library(tibble)
 
 # Define UI for application that draws a histogram
@@ -46,7 +45,9 @@ ui <- fluidPage(
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("Plot")
+      plotOutput("Plot"),
+      tableOutput("table"),
+      tableOutput("table2")
     )
   )
 )
@@ -62,13 +63,18 @@ server <- function(input, output) {
     k <- input$k
     seq <- c(1:length(p))
     cum <- p + g*n/k # PO
-    
-    df <- tibble(seq, p, g, cum)
-    diffG <- (g[seq] - g[seq+1])/n 
+    diffG <- (g[seq] - g[seq+1])/k 
     diffP <- p[seq+1] - p[seq] 
-    # ne <- df$seq[diffG > diffP & !is.na(diffG > diffP)] # NE
-    ne <- df$seq[p+(g/n) == max(p+(g/n))]
+    PGK <- diffP-diffG
+    SGN <- (n-1)*diffG
     
+    df <- tibble(seq, p, g, cum, PGK)
+
+
+    # ne <- df$seq[diffG > diffP & !is.na(diffG > diffP)] # NE
+    ne <- df$seq[p+(g/k) == max(p+(g/k))]
+    #ne <- df$seq[PGK == min(PGK, na.rm = T) & !is.na(PGK)]
+
     
     ggplot() +
       geom_line(aes(x = seq, y = p), linetype = "dashed",color = "red") +
@@ -78,10 +84,52 @@ server <- function(input, output) {
       scale_x_continuous(breaks = c(seq)) +
       geom_text(label ="NE", aes(x = ne, y = p[ne]), nudge_y = 3) +
       geom_text(label ="PO", aes(x = df$seq[df$cum == max(df$cum)], y = max(df$cum)), nudge_y = 5) +
-      labs(title = "", subtitle = "dotted = public; dashed = private; solid = aggregat") +
+      labs(title = "", subtitle = "dotted = public; dashed = private; solid = aggregated") +
       xlab("Option") +
       ylab("Contribution (private, public, acc, added") +
       theme_bw()
+  })
+  
+  
+  output$table <- renderTable({
+    p <- as.numeric(unlist(strsplit(input$p_i, ","))) #c(0,90,150,175,200,209,220)
+    g <- as.numeric(unlist(strsplit(input$g_i, ","))) # c(300,250,200,150,100,50,0)  #as.numeric(unlist(strsplit(input$vec1,",")))
+    n <- input$n 
+    k <- input$k
+    seq <- c(1:length(p))
+    p_plus_g <- p + g*n/k # PO
+#    diffG <- (g[seq] - g[seq+1])/k
+#    diffP <- p[seq+1] - p[seq] 
+#    PGK <- diffP-diffG
+#    SGK <- (n-1)*diffG
+    
+    df <- tibble(seq, p, g, p_plus_g)
+    # ne <- df$seq[diffG > diffP & !is.na(diffG > diffP)] # NE
+    #ne <- df$seq[p+(g/n) == max(p+(g/n))]
+    df
+  })
+  
+  
+  output$table2 <- renderTable({
+    p <- as.numeric(unlist(strsplit(input$p_i, ","))) #c(0,90,150,175,200,209,220)
+    g <- as.numeric(unlist(strsplit(input$g_i, ","))) # c(300,250,200,150,100,50,0)  #as.numeric(unlist(strsplit(input$vec1,",")))
+    n <- input$n 
+    k <- input$k
+    seq <- c(1:length(p))
+    p_plus_g <- p + g*n/k # PO
+    diffG <- (g[seq] - g[seq+1])/k 
+    diffP <- p[seq+1] - p[seq] 
+    PGK <- diffP-diffG
+    SGN <- (n-1)*diffG
+    df <- tibble(seq, diffP,diffG, PGK, SGN)
+    
+    seq2 <- paste(seq+1, "->", seq)
+    
+    df <- tibble(seq2, diffP,diffG, PGK, SGN)
+    df <- df[-nrow(df),]
+    # ne <- df$seq[diffG > diffP & !is.na(diffG > diffP)] # NE
+ #   ne <- df$seq[p+(g/n) == max(p+(g/n))]
+    df
   })
 }
 
