af85dcb642476fa297eede788d0327bfce44c1b6
Zirys1 <hen.b@posteo.de>
2018-11-21 16:48:17 +0100

    include color coding and conditional tables

--- ne_po_pgg/app.R
+++ ne_po_pgg/app.R
@@ -10,6 +10,7 @@
 library(shiny)
 library(ggplot2)
 library(tibble)
+library(formattable)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -35,29 +36,36 @@ ui <- fluidPage(
                   step = 1)
       ,
       checkboxInput("y",
-                    "Choice menus slider?",
+                    "Manual choice menu input",
                     value = FALSE)
       ,
-      textInput("g_i",
-                "Choice menu: Payment options to public good",
-                value = "300,250,200,150,100,50,0")
+      conditionalPanel(
+        condition = "input.y == 1",
+        textInput("g_i",
+                  "Choice menu: Payment options to public good",
+                  value = "300,250,200,150,100,50,0")
+        ,
+        textInput("p_i",
+                  "Choice menu: Payment options to private good",
+                  value = "0,90,150,175,200,209,220") 
+      )
       ,
-      textInput("p_i",
-                "Choice menu: Payment options to private good",
-                value = "0,90,150,175,200,209,220") 
-      ,
-      sliderInput("cs",
-                  "Private choice sets",
-                  min = 1,
-                  max = 6,
-                  value = 1,
-                  step = 1)
+      conditionalPanel(
+        condition = "input.y == 0",
+        sliderInput("cs",
+                    "Private choice sets",
+                    min = 1,
+                    max = 6,
+                    value = 1,
+                    step = 1) 
+      )
     ),
+
     
     # Show a plot of the generated distribution
     mainPanel(
       plotOutput("Plot"),
-      tableOutput("table"),
+      formattableOutput("table"),
       tableOutput("table2")
     )
   )
@@ -70,35 +78,35 @@ server <- function(input, output) {
 
     p1 <- as.numeric(unlist(strsplit(input$p_i, ","))) 
     g1 <- as.numeric(unlist(strsplit(input$g_i, ","))) 
-    if(input$y == TRUE){   
+    if(input$y == FALSE){   
     if(input$cs == 1){    
     p <- as.numeric(c(0,110,250,300,350,370,380)) 
     g <- as.numeric(c(600,500,400,300,200,100,0)) 
     }
     if(input$cs == 2){    
-      p <- as.numeric(c(0,120,260,310,360,380,390)) 
+      p <- as.numeric(c(0,120,260,310,360,379,388)) 
       g <- as.numeric(c(600,500,400,300,200,100,0)) 
     }
     if(input$cs == 3){    
-      p <- as.numeric(c(0,130,270,320,370,390,400)) 
+      p <- as.numeric(c(0,130,270,320,370,388,396)) 
       g <- as.numeric(c(600,500,400,300,200,100,0)) 
     }
     if(input$cs == 4){    
-      p <- as.numeric(c(0,140,280,330,380,400,410)) 
+      p <- as.numeric(c(0,140,280,330,380,397,404)) 
       g <- as.numeric(c(600,500,400,300,200,100,0)) 
     }
     if(input$cs == 5){    
-      p <- as.numeric(c(0,150,290,340,390,410,420)) 
+      p <- as.numeric(c(0,150,290,340,390,406,412)) 
       g <- as.numeric(c(600,500,400,300,200,100,0)) 
     }
     if(input$cs == 6){    
-      p <- as.numeric(c(0,160,300,350,400,420,430)) 
+      p <- as.numeric(c(0,160,300,350,400,415,420)) 
       g <- as.numeric(c(600,500,400,300,200,100,0)) 
     }
   }
     n <- input$n 
     k <- input$k
-    if(input$y == FALSE){
+    if(input$y == TRUE){
       p <- p1
       g <- g1
     }
@@ -118,8 +126,8 @@ server <- function(input, output) {
 
     
     ggplot() +
-      geom_line(aes(x = seq, y = p), linetype = "dashed",color = "red") +
-      geom_line(aes(x = seq, y = g), linetype = "dotted", color = "green") +
+      geom_line(aes(x = seq, y = p), linetype = "dashed",color = "#FA614B") +
+      geom_line(aes(x = seq, y = g), linetype = "dotted", color = "#4A7023") +
       geom_line(aes(x = seq, y = cum), linetype = "solid", color = "black") +
       #  geom_line(aes(x = seq, y = ne), linetype = "solid", color = "red") +
       scale_x_continuous(breaks = c(seq)) +
@@ -132,38 +140,38 @@ server <- function(input, output) {
   })
   
   
-  output$table <- renderTable({
+  output$table <- renderFormattable({
     p1 <- as.numeric(unlist(strsplit(input$p_i, ","))) 
     g1 <- as.numeric(unlist(strsplit(input$g_i, ","))) 
-    if(input$y == TRUE){   
+    if(input$y == FALSE){   
       if(input$cs == 1){    
         p <- as.numeric(c(0,110,250,300,350,370,380)) 
         g <- as.numeric(c(600,500,400,300,200,100,0)) 
       }
       if(input$cs == 2){    
-        p <- as.numeric(c(0,120,260,310,360,380,390)) 
+        p <- as.numeric(c(0,120,260,310,360,379,388)) 
         g <- as.numeric(c(600,500,400,300,200,100,0)) 
       }
       if(input$cs == 3){    
-        p <- as.numeric(c(0,130,270,320,370,390,400)) 
+        p <- as.numeric(c(0,130,270,320,370,388,396)) 
         g <- as.numeric(c(600,500,400,300,200,100,0)) 
       }
       if(input$cs == 4){    
-        p <- as.numeric(c(0,140,280,330,380,400,410)) 
+        p <- as.numeric(c(0,140,280,330,380,397,404)) 
         g <- as.numeric(c(600,500,400,300,200,100,0)) 
       }
       if(input$cs == 5){    
-        p <- as.numeric(c(0,150,290,340,390,410,420)) 
+        p <- as.numeric(c(0,150,290,340,390,406,412)) 
         g <- as.numeric(c(600,500,400,300,200,100,0)) 
       }
       if(input$cs == 6){    
-        p <- as.numeric(c(0,160,300,350,400,420,430)) 
+        p <- as.numeric(c(0,160,300,350,400,415,420)) 
         g <- as.numeric(c(600,500,400,300,200,100,0)) 
       }
     }
     n <- input$n 
     k <- input$k
-    if(input$y == FALSE){
+    if(input$y == TRUE){
       p <- p1
       g <- g1
     }
@@ -173,8 +181,12 @@ server <- function(input, output) {
 #    diffP <- p[seq+1] - p[seq] 
 #    PGK <- diffP-diffG
 #    SGK <- (n-1)*diffG
+    p_plus_g_div_k <- p + g/k
     
-    df <- tibble(seq, p, g, p_plus_g)
+    df <- tibble(seq, p, g, p_plus_g, p_plus_g_div_k)
+    df <- formattable(df, align = c("l", rep("r", ncol(df) - 1)),
+                list("p_plus_g" = proportion_bar(color= ifelse(p_plus_g == max(p_plus_g), "#457371", "#4A7023")),
+                     "p_plus_g_div_k" = proportion_bar(color = ifelse(p_plus_g_div_k == max(p_plus_g_div_k), "#457371", "#FA614B"))))
     # ne <- df$seq[diffG > diffP & !is.na(diffG > diffP)] # NE
     #ne <- df$seq[p+(g/n) == max(p+(g/n))]
     df
@@ -184,35 +196,35 @@ server <- function(input, output) {
   output$table2 <- renderTable({
     p1 <- as.numeric(unlist(strsplit(input$p_i, ","))) 
     g1 <- as.numeric(unlist(strsplit(input$g_i, ","))) 
-    if(input$y == TRUE){   
+    if(input$y == FALSE){   
       if(input$cs == 1){    
         p <- as.numeric(c(0,110,250,300,350,370,380)) 
         g <- as.numeric(c(600,500,400,300,200,100,0)) 
       }
       if(input$cs == 2){    
-        p <- as.numeric(c(0,120,260,310,360,380,390)) 
+        p <- as.numeric(c(0,120,260,310,360,379,388)) 
         g <- as.numeric(c(600,500,400,300,200,100,0)) 
       }
       if(input$cs == 3){    
-        p <- as.numeric(c(0,130,270,320,370,390,400)) 
+        p <- as.numeric(c(0,130,270,320,370,388,396)) 
         g <- as.numeric(c(600,500,400,300,200,100,0)) 
       }
       if(input$cs == 4){    
-        p <- as.numeric(c(0,140,280,330,380,400,410)) 
+        p <- as.numeric(c(0,140,280,330,380,397,404)) 
         g <- as.numeric(c(600,500,400,300,200,100,0)) 
       }
       if(input$cs == 5){    
-        p <- as.numeric(c(0,150,290,340,390,410,420)) 
+        p <- as.numeric(c(0,150,290,340,390,406,412)) 
         g <- as.numeric(c(600,500,400,300,200,100,0)) 
       }
       if(input$cs == 6){    
-        p <- as.numeric(c(0,160,300,350,400,420,430)) 
+        p <- as.numeric(c(0,160,300,350,400,415,420)) 
         g <- as.numeric(c(600,500,400,300,200,100,0)) 
       }
     }
     n <- input$n 
     k <- input$k
-    if(input$y == FALSE){
+    if(input$y == TRUE){
       p <- p1
       g <- g1
     }
